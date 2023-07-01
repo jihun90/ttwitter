530c9012d2ff4ddc3d07f3789226abc401702e41
jihun90 <jihun90.kim@icloud.com>
2023-07-01 17:43:43 +0900

    firebase 설치 및 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import firebase from './firebase.ts'
+
+console.log(firebase);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
