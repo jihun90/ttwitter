92c9c0d26697dc7c1813994135cc0b7eab3c67ca
jihun90 <jihun90.kim@icloud.com>
2023-07-05 09:05:04 +0900

    Auth Input Compnent 생성

---
+++ src/components/AuthInput.tsx
@@ -0,0 +1,30 @@
+import { HTMLInputTypeAttribute, ReactNode, SetStateAction } from "react";
+
+type State = string;
+type Dispatch = React.Dispatch<SetStateAction<string>>;
+
+type InputComponentProps = {
+    inputType: HTMLInputTypeAttribute;
+    state: State;
+    dispatch: Dispatch;
+};
+
+function AuthInput({ inputType, state, dispatch }: InputComponentProps): ReactNode {
+    function onChange(event: React.FormEvent<HTMLInputElement>): void {
+        if (!dispatch || dispatch === null) throw new Error("Cannot find InputTypeDispatchProvider");
+        dispatch(event.currentTarget.value);
+    }
+
+    return (
+        <input
+            name={inputType}
+            type={inputType}
+            placeholder={inputType}
+            required
+            value={state}
+            onChange={onChange}
+        />
+    );
+}
+
+export default AuthInput;
