26eea830c428e6f005af6d90688f50fb34b0b2f2
jihun90 <jihun90.kim@icloud.com>
2023-07-08 19:08:28 +0900

    prettier 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  root: true,
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module",parser: '@typescript-eslint/parser', },
+  plugins: ['prettier', '@typescript-eslint',"react-refresh"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth:42,
+        printWidth: 100,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
   },
-}
+};
