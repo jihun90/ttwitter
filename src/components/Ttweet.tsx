51c7c12790b20688c520296d1ceeae793a3e0f46
jihun90 <jihun90.kim@icloud.com>
2023-07-21 15:19:14 +0900

    update Form 코드 정리

--- src/components/Ttweet.tsx
+++ src/components/Ttweet.tsx
@@ -1,8 +1,8 @@
-import { CollectionID, MessageInfo } from '@/models/collectionContainer';
 import { AuthService } from '@/services/firebase/authService';
 import DeleteButton from '@/components/DeleteButton';
 import React, { useState } from 'react';
-import { DBService } from '@/services/firebase/dbService';
+import { MessageInfo } from '@/models/collectionContainer';
+import UpdateForm from './UpdateForm';
 
 type Prop = {
     ttweetObj: MessageInfo;
@@ -10,39 +10,14 @@ type Prop = {
 
 function Ttweet({ ttweetObj }: Prop): React.JSX.Element {
     const [editting, setEditting] = useState(false);
-    const [newTtweet, setNewTtweet] = useState(ttweetObj.text);
-
     const toggleEdtting = () => setEditting(pre => !pre);
-
     const isOwner: boolean = ttweetObj.createdBy === (AuthService.GetInstance().user?.uid ?? '');
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = event;
 
-        setNewTtweet(value);
-    };
-    const onSubmit = (event: React.FormEvent<HTMLInputElement>) => {
-        event.preventDefault();
-        const userId = AuthService.GetInstance().user?.uid ?? '';
-        const ttweet: MessageInfo = { id: ttweetObj.id, text: newTtweet, createdAt: Date.now(), createdBy: userId };
-        DBService.GetInstance().Collection[CollectionID.ttweet].update(ttweet);
-    };
     return (
         <div>
             {editting ? (
                 <>
-                    <form>
-                        <h4>{ttweetObj.text}</h4>
-                        <input
-                            type="text"
-                            onChange={onChange}
-                            placeholder="Edit your ttweet"
-                            value={newTtweet}
-                            required
-                        />
-                        <input type="submit" onSubmit={onSubmit} value="Update ttweett" required />
-                    </form>
+                    <UpdateForm ttweetObj={ttweetObj} />
                     <button onClick={toggleEdtting}>Cancel</button>
                 </>
             ) : (
