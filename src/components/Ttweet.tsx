5186c1be09f2a482355b4f4a25eac9d8175b9a72
jihun90 <jihun90.kim@icloud.com>
2023-07-21 17:05:30 +0900

    코드 정리

--- src/components/Ttweet.tsx
+++ src/components/Ttweet.tsx
@@ -11,28 +11,30 @@ type Prop = {
 function Ttweet({ ttweetObj }: Prop): React.JSX.Element {
     const [editting, setEditting] = useState(false);
     const toggleEdtting = () => setEditting(pre => !pre);
-    const isOwner: boolean = ttweetObj.createdBy === (AuthService.GetInstance().user?.uid ?? '');
 
-    return (
-        <div>
-            {editting ? (
-                <>
-                    <UpdateForm ttweetObj={ttweetObj} />
-                    <button onClick={toggleEdtting}>Cancel</button>
-                </>
-            ) : (
-                <>
-                    {isOwner && (
-                        <div>
-                            <h4>{ttweetObj.text}</h4>
-                            <DeleteButton ttweetobj={ttweetObj} />
-                            <button onClick={toggleEdtting}>Edit Ttweet</button>
-                        </div>
-                    )}
-                </>
-            )}
-        </div>
-    );
+    const isOwner: boolean = ttweetObj.createdBy === (AuthService.GetInstance().user?.uid ?? '');
+    if (isOwner) return <></>;
+
+    const UpdateView = () => {
+        return (
+            <>
+                <UpdateForm ttweetObj={ttweetObj} />
+                <button onClick={toggleEdtting}>Cancel</button>
+            </>
+        );
+    };
+
+    const DefualtView = () => {
+        return (
+            <div>
+                <h4>{ttweetObj.text}</h4>
+                <DeleteButton ttweetobj={ttweetObj} />
+                <button onClick={toggleEdtting}>Edit Ttweet</button>
+            </div>
+        );
+    };
+
+    return <div>{editting ? <UpdateView /> : <DefualtView />}</div>;
 }
 
 export default Ttweet;
