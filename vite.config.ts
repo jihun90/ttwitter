9a536a7be52a3fb811f5d67589f2aa2daf361674
jihun90 <jihun90.kim@icloud.com>
2023-08-04 23:14:22 +0900

    vite에서 배포되도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: UserConfigExport = {
-    base: "./",
+    base: "/ttwitter/",
     plugins: [react(), tsconfigPaths()],
     resolve: {
         alias: [
